97c0aa7eb1d2d3b6cfc01b8f7cdd9c5b58909b34
collins kiprotich <collinskiprotich@gmail.com>
2023-04-13 22:38:51 +0300

    Fix Linter error

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-/* eslint-disable no-param-reasign*/
+/* eslint-disable no-param-reasign */
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState: {
